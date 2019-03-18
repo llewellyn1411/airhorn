283b686021d8c3c6621ac430e53baa547bcc07bb
llewellyn.c.collins <llewellyn.c.collins@gmail.com>
2019-03-18 21:47:04 +0000

    Offlline airhorn

---
+++ app/sw.js
@@ -0,0 +1,29 @@
+const cacheName = 'airhorner';
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(cacheName)
+            .then(cache=>{
+                return cache.addAll(
+                    [
+                        '/',
+                        '/index.html',
+                        '/index.html?homescreen=1',
+                        '/?homescreen=1',
+                        '/styles/main.css',
+                        '/scripts/main.min.js',
+                        '/sounds/airhorn.mp3'
+                    ]
+                )
+            })
+    );
+});
+
+self.addEventListener('fetch', event =>{
+    event.respondWith(
+        caches.match(event.request)
+            .then(response =>{
+                return response || fetch(event.request);
+            })
+    );
+});
